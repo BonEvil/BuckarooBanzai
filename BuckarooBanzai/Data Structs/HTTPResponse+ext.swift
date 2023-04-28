7ddd2e91d2e661944afb98cd04a3e447eebcf810
Daniel Person <daniel.person@yahoo.com>
2023-04-28 07:45:22 -0400

    added JSONDecoder key decoding strategy

--- BuckarooBanzai/Data Structs/HTTPResponse+ext.swift
+++ BuckarooBanzai/Data Structs/HTTPResponse+ext.swift
@@ -17,11 +17,14 @@ extension HTTPResponse {
     ///let myObject: MyObject = try response.decodeBodyData()
     /// ```
     /// - Returns: The Decodable object specified or throws an error if there was no data returned or if the data could not be parsed to the specified object.
-    public func decodeBodyData<T: Decodable>() throws -> T {
+    public func decodeBodyData<T: Decodable>(convertFromSnakeCase: Bool = false) throws -> T {
         guard let data = body else {
             throw BBError.decoder([NSLocalizedDescriptionKey: "No body data found."])
         }
         let decoder = JSONDecoder()
+        if convertFromSnakeCase {
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+        }
         let object = try decoder.decode(T.self, from: data)
         return object
     }
